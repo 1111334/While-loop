210a83b6507598618c66d2ec5480502e7914a88b
Mike <109431533+1111334@users.noreply.github.com>
2023-07-24 15:18:45 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,23 @@
 
 
-import java.util.Random;
+
 
 public class Main {
     public static void main(String[] args) {
-
+        int days = 0;
         int totPounds = 10000;
         while (totPounds > 0) {
 
             double todayrate = 1 + (Math.random() * 0.2);
+            days++;
 
             if (todayrate > 1.15) {
                 totPounds = totPounds - 1000;
                 System.out.println("the exchange rate " + todayrate + " remained " + totPounds);
-
             }
         }
+        System.out.println("sold all the pounds in " + days + " days");
+
     }
 }
 
